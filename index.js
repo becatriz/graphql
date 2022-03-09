3c30675f6dbdc9ebe3b1299b8b21f5ec90dbd369
Rebeca <rebeca.cruz@before.com.br>
2022-03-09 10:02:30 -0400

    fix: add news queries API

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+const users = [
+  {
+    id: 1,
+    name: "Bia Liz",
+    email: "bia@gmail.com",
+    age: 29,
+    profile_id: 1,
+  },
+  {
+    id: 2,
+    name: "Rosa Kou",
+    email: "kou@gmail.com",
+    age: 48,
+    profile_id: 2,
+  },
+  {
+    id: 3,
+    name: "Wiloyu Iuu",
+    email: "iuu@gmail.com",
+    age: 18,
+    profile_id: 1,
+  },
+];
+
+const profiles = [
+  { id: 1, name: "Comun" },
+  { id: 2, name: "Administrador" },
+];
+
 const typeDefs = gql`
   scalar Date
 
+  type Profile {
+    id: Int
+    name: String
+  }
+
   type User {
-    id: ID!
+    id: Int!
     name: String!
     email: String!
     age: Int
     salary: Float
     vip: Boolean
+    profile: Profile
   }
 
   type Product {
@@ -19,28 +54,41 @@ const typeDefs = gql`
     priceWithDiscount: Float
   }
 
-  # Pontos de entrada da API
+  # API Entry Points
   type Query {
-    currentTime: Date! # Exclamação significa que o retorno é obrigatorio
+    currentTime: Date! # Exclamation means that the return is mandatory
     loggedInUser: User!
     featuredProduct: Product!
     lotteryNumbers: [Int]!
+    users: [User]!
+    user(id: Int): User
+    profiles: [Profile]!
+    profile(id: Int): Profile
   }
 `;
 
 const resolvers = {
-  User: {
-    salary(usuario) {
-      return usuario.real_salary;
-    },
-  },
-
   Product: {
     priceWithDiscount(parent) {
       if (parent.discount) {
         return parent.price * (1 - parent.discount);
       }
       return parent.price;
+    },
+  },
+
+  User: {
+    salary(user) {
+      return user.real_salary;
+    },
+
+    profile(user) {
+      // Relationship between two different data types
+      const selected = profiles.find(
+        (profile) => profile.id === user.profile_id
+      );
+
+      return selected ? selected : null;
     },
   },
 
@@ -73,6 +121,20 @@ const resolvers = {
           .map((n) => parseInt(Math.random() * 60 + 1))
           .sort(ascendingOrder)
       );
+    },
+    users() {
+      return users;
+    },
+    user(_, { id }) {
+      const selected = users.find((user) => user.id === id);
+      return selected ? selected : null;
+    },
+    profiles() {
+      return profiles;
+    },
+    profile(_, { id }) {
+      const selected = profiles.find((profile) => profile.id === id);
+      return selected ? selected : null;
     },
   },
 };
