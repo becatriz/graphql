461cd58ead773b53ee7a38a5b0828e98f272b78e
Rebeca <becatriz7@gmail.com>
2022-03-09 20:44:34 -0400

    fix: add inputs mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,23 @@
 const { users, nextId } = require("../data/db");
 
+const NOT_FOUND = -1;
+
 function validatesExistingEmail(email) {
   return users.some((user) => user.email === email);
+}
+
+function findUserIndex(filter) {
+  if (!filter?.id) return NOT_FOUND;
+
+  const { id, email } = filter;
+
+  if (id) {
+    return users.findIndex((user) => user.id === id);
+  } else if (email) {
+    return users.findIndex((user) => user.email === email);
+  }
+
+  return NOT_FOUND;
 }
 
 function newUser(_, { data }) {
@@ -23,8 +39,8 @@ function newUser(_, { data }) {
   return newUser;
 }
 
-function removeUser(_, { id }) {
-  const userIndex = users.findIndex((user) => user.id === id);
+function removeUser(_, { filter }) {
+  const userIndex = findUserIndex(filter);
 
   if (userIndex < 0) return null;
 
@@ -33,14 +49,14 @@ function removeUser(_, { id }) {
   return userRemoved ? userRemoved[0] : null;
 }
 
-function updateUser(_, args) {
-  const userIndex = users.findIndex((user) => user.id === args.id);
+function updateUser(_, { filter }) {
+  const userIndex = findUserIndex(filter);
 
   if (userIndex < 0) return null;
 
   const user = {
     ...users[userIndex],
-    ...args,
+    ...filter,
   };
 
   users.splice(userIndex, 1, user);
